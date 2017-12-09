7bad7263fffc58fb131a943fabc832d00b09a9e1
shekang <m18290048583@163.com>
2017-12-09 23:22:12 +0800

    11

--- js/mouse-effect.js
+++ js/mouse-effect.js
@@ -23,7 +23,7 @@ var ball = {
    balls = [],
    alpha_f = 0.03,
    alpha_phase = 0,
-    
+
 // Line
    link_line_width = 0.8,
    dis_limit = 260,
@@ -66,7 +66,7 @@ function randomArrayItem(arr){
 function randomNumFrom(min, max){
     return Math.random()*(max - min) + min;
 }
-console.log(randomNumFrom(0, 10));
+// console.log(randomNumFrom(0, 10));
 // Random Ball
 function getRandomBall(){
     var pos = randomArrayItem(['top', 'right', 'bottom', 'left']);
@@ -140,23 +140,23 @@ function updateBalls(){
     Array.prototype.forEach.call(balls, function(b){
         b.x += b.vx;
         b.y += b.vy;
-        
+
         if(b.x > -(50) && b.x < (can_w+50) && b.y > -(50) && b.y < (can_h+50)){
            new_balls.push(b);
         }
-        
+
         // alpha change
         b.phase += alpha_f;
         b.alpha = Math.abs(Math.cos(b.phase));
         // console.log(b.alpha);
     });
-    
+
     balls = new_balls.slice(0);
 }
 
 // loop alpha
 function loopAlphaInf(){
-    
+
 }
 
 // Draw lines
@@ -164,15 +164,15 @@ function renderLines(){
     var fraction, alpha;
     for (var i = 0; i < balls.length; i++) {
         for (var j = i + 1; j < balls.length; j++) {
-           
+
            fraction = getDisOf(balls[i], balls[j]) / dis_limit;
-            
+
            if(fraction < 1){
                alpha = (1 - fraction).toString();
 
                ctx.strokeStyle = 'rgba(150,150,150,'+alpha+')';
                ctx.lineWidth = link_line_width;
-               
+
                ctx.beginPath();
                ctx.moveTo(balls[i].x, balls[i].y);
                ctx.lineTo(balls[j].x, balls[j].y);
@@ -187,7 +187,7 @@ function renderLines(){
 function getDisOf(b1, b2){
     var  delta_x = Math.abs(b1.x - b2.x),
        delta_y = Math.abs(b1.y - b2.y);
-    
+
     return Math.sqrt(delta_x*delta_x + delta_y*delta_y);
 }
 
@@ -201,15 +201,15 @@ function addBallIfy(){
 // Render
 function render(){
     ctx.clearRect(0, 0, can_w, can_h);
-    
+
     renderBalls();
-    
+
     renderLines();
-    
+
     updateBalls();
-    
+
     addBallIfy();
-    
+
     window.requestAnimationFrame(render);
 }
 
@@ -231,12 +231,12 @@ function initBalls(num){
 function initCanvas(){
     canvas.setAttribute('width', window.innerWidth);
     canvas.setAttribute('height', window.innerHeight);
-    
+
     can_w = parseInt(canvas.getAttribute('width'));
     can_h = parseInt(canvas.getAttribute('height'));
 }
 window.addEventListener('resize', function(e){
-    console.log('Window Resize...');
+    // console.log('Window Resize...');
     initCanvas();
 });
 
@@ -249,12 +249,12 @@ goMovie();
 
 // Mouse effect
 canvas.addEventListener('mouseenter', function(){
-    console.log('mouseenter');
+    // console.log('mouseenter');
     mouse_in = true;
     balls.push(mouse_ball);
 });
 canvas.addEventListener('mouseleave', function(){
-    console.log('mouseleave');
+    // console.log('mouseleave');
     mouse_in = false;
     var new_balls = [];
     Array.prototype.forEach.call(balls, function(b){
@@ -270,18 +270,3 @@ canvas.addEventListener('mousemove', function(e){
     mouse_ball.y = e.pageY;
     // console.log(mouse_ball);
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
